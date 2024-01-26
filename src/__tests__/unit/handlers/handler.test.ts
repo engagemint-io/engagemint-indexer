31d069ba617a9534bc03aff22407ffa3353eb0cb
Denys Sinyakov <denys.sinyakov@gmail.com>
2024-01-25 16:11:34 -0800

    - first test done

--- src/__tests__/unit/handlers/handler.test.ts
+++ src/__tests__/unit/handlers/handler.test.ts
@@ -3,6 +3,64 @@ import { handler } from '../../../handler';
 import { jest } from '@jest/globals';
 import { Context, ScheduledEvent } from 'aws-lambda';
 
+describe('Handler tests', function() {
+	const testTicker = 'TESTTICKER';
+	const username = 'TESTUSER';
+	const twitterId = '1234567890';
+	// Add a new test to assess the handler
+	it('Verifies the handler calculates user points based on 1 tweet', async () => {
+		mockFetchAllTickerConfigs.mockImplementation(() => [{
+			ticker: { S: testTicker },
+			epoch_start_date_utc: { S: '2024-01-16T00:00:00+00:00' },
+			epoch_length_days: { N: '7' },
+			like_multiplier: { N: '2' },
+			retweet_multiplier: { N: '3' },
+			view_multiplier: { N: '4' },
+			video_view_multiplier: { N: '5' },
+			quote_multiplier: { N: '6' }
+		}]);
+
+		mockFetchUsersForTicker.mockImplementation(() => [{ twitter_id: twitterId }]);
+		mockGetUsernameById.mockImplementation(() => username);
+		mockFetchUserTweetsWithTicker.mockImplementation(() => {
+			return {
+				tweets: [{
+					text: 'this is a tweet about $' + testTicker,
+					public_metrics: { like_count: 1, quote_count: 2, retweet_count: 3, impression_count: 4 },
+					attachments: { media_keys: ['123'] }
+				}], includes: { media: [{ public_metrics: { view_count: 7 }, media_key: '123' }] }
+			};
+		});
+
+		mockGetCurrentTime.mockImplementation(() => new Date('2024-01-18T00:00:00+00:00'));
+		mockGetCurrentEpochNumber.mockImplementation(() => 1);
+		mockGetCurrentEpochStartDate.mockImplementation(() => new Date('2024-01-16T00:00:00+00:00'));
+
+		await handler(payload, null as unknown as Context);
+
+		const likePoints = 2;
+		const quotePoints = 12;
+		const retweetPoints = 9;
+		const viewPoints = 16;
+		const videoViewPoints = 35;
+		const totalPoints = likePoints + quotePoints + retweetPoints + viewPoints + videoViewPoints;
+
+		expect(mockPersistUserStatsInBulk).toHaveBeenCalledWith([{
+			username: username,
+			twitter_id: twitterId,
+			ticker: testTicker,
+			epoch: "1",
+			likePoints: likePoints,
+			quotePoints: quotePoints,
+			retweetPoints: retweetPoints,
+			viewPoints: viewPoints,
+			videoViewPoints: videoViewPoints,
+			totalPoints: totalPoints,
+			rank: 1
+		}]);
+	});
+});
+
 // Create a sample payload with CloudWatch scheduled event message format
 const payload: ScheduledEvent<any> = {
 	'id': 'cdc73f9d-aea9-11e3-9d5a-835b769c0d9c',
@@ -17,7 +75,6 @@ const payload: ScheduledEvent<any> = {
 	'detail': {},
 	version: ''
 };
-
 
 jest.mock('../../../database', () => {
 	return {
@@ -49,65 +106,18 @@ jest.mock('../../../twitter', () => {
 let mockGetUsernameById = jest.fn();
 let mockFetchUserTweetsWithTicker = jest.fn();
 
-afterEach(() => {
-	jest.restoreAllMocks();
-});
-
-describe('Handler tests', function() {
-	const testTicker = 'TESTTICKER';
-	const username = 'TESTUSER';
-	const twitterId = '1234567890';
-	// Add a new test to assess the handler
-	it('Verifies the handler calls the correct methods', async () => {
-		const mockDate = new Date('2024-01-20T00:00:00+00:00'); // This can be any date you want
-		jest.spyOn(global, 'Date').mockImplementation(() => mockDate as any);
-		mockFetchAllTickerConfigs.mockImplementation(() => [{
-			ticker: { S: testTicker },
-			epoch_start_date_utc: { S: '2024-01-16T00:00:00+00:00' },
-			epoch_length_days: { N: '7' },
-			like_multiplier: { N: '2' },
-			retweet_multiplier: { N: '3' },
-			view_multiplier: { N: '4' },
-			video_view_multiplier: { N: '5' },
-			quote_multiplier: { N: '6' }
-		}]);
-
-		mockFetchUsersForTicker.mockImplementation(() => [{ twitter_id: twitterId }]);
-		mockGetUsernameById.mockImplementation(() => username);
-		mockFetchUserTweetsWithTicker.mockImplementation(() => {
+jest.mock('../../../time', () => {
+	return {
+		TimeService: jest.fn().mockImplementation(() => {
 			return {
-				tweets: [{
-					text: 'this is a tweet about $' + testTicker,
-					public_metrics: { like_count: 1, quote_count: 2, retweet_count: 3, impression_count: 4 },
-					attachments: { media_keys: ['123'] }
-				}], includes: { media: [{ public_metrics: { view_count: 7 }, media_key: '123' }] }
+				getCurrentTime: mockGetCurrentTime,
+				getCurrentEpochNumber: mockGetCurrentEpochNumber,
+				getCurrentEpochStartDate: mockGetCurrentEpochStartDate
 			};
-		});
-
-		await handler(payload, null as unknown as Context);
-
-		const likePoints = 2;
-		const quotePoints = 12;
-		const retweetPoints = 9;
-		const viewPoints = 16;
-		const videoViewPoints = 35;
-		const totalPoints = likePoints + quotePoints + retweetPoints + viewPoints + videoViewPoints;
-
-		// ... rest of your test code
-
-		expect(mockPersistUserStatsInBulk).toHaveBeenCalledWith([{
-			username: username,
-			twitter_id: twitterId,
-			ticker: testTicker,
-			epoch: 1,
-			likePoints: likePoints,
-			quotePoints: quotePoints,
-			retweetPoints: retweetPoints,
-			viewPoints: viewPoints,
-			videoViewPoints: videoViewPoints,
-			totalPoints: totalPoints,
-			rank: 1
-		}]);
-	});
-
+		})
+	};
 });
+
+let mockGetCurrentTime = jest.fn();
+let mockGetCurrentEpochNumber = jest.fn();
+let mockGetCurrentEpochStartDate = jest.fn();
