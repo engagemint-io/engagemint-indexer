03542a4ad8cd14e85e2fa743b7ca6999e9556641
Denys Sinyakov <denys.sinyakov@gmail.com>
2024-01-25 16:30:02 -0800

    - add couple negative test cases

--- src/__tests__/unit/handlers/handler.test.ts
+++ src/__tests__/unit/handlers/handler.test.ts
@@ -4,45 +4,109 @@ import { jest } from '@jest/globals';
 import { Context, ScheduledEvent } from 'aws-lambda';
 
 describe('Handler tests', function() {
+
+	const username = 'testuser';
+	const twitterId = '123';
 	const testTicker = 'TESTTICKER';
-	const username = 'TESTUSER';
-	const twitterId = '1234567890';
-	// Add a new test to assess the handler
-	it('Verifies the handler calculates user points based on 1 tweet', async () => {
+
+	const likeMultiplier = 2;
+	const quoteMultiplier = 6;
+	const retweetMultiplier = 3;
+	const viewMultiplier = 4;
+	const videoViewMultiplier = 5;
+
+	it('Verifies no user points persisted there are no tweets for a user', async () => {
 		mockFetchAllTickerConfigs.mockImplementation(() => [{
 			ticker: { S: testTicker },
 			epoch_start_date_utc: { S: '2024-01-16T00:00:00+00:00' },
 			epoch_length_days: { N: '7' },
-			like_multiplier: { N: '2' },
-			retweet_multiplier: { N: '3' },
-			view_multiplier: { N: '4' },
-			video_view_multiplier: { N: '5' },
-			quote_multiplier: { N: '6' }
+			like_multiplier: { N: likeMultiplier.toString() },
+			retweet_multiplier: { N: retweetMultiplier.toString() },
+			view_multiplier: { N: viewMultiplier.toString() },
+			video_view_multiplier: { N: videoViewMultiplier.toString() },
+			quote_multiplier: { N: quoteMultiplier.toString() }
 		}]);
 
 		mockFetchUsersForTicker.mockImplementation(() => [{ twitter_id: twitterId }]);
-		mockGetUsernameById.mockImplementation(() => username);
+		mockGetCurrentTime.mockImplementation(() => new Date('2024-01-18T00:00:00+00:00'));
+		mockGetCurrentEpochNumber.mockImplementation(() => 1);
+		mockGetCurrentEpochStartDate.mockImplementation(() => new Date('2024-01-16T00:00:00+00:00'));
+
+		mockFetchUserTweetsWithTicker.mockImplementation(() => {
+			return {
+				tweets: []
+			};
+		});
+
+		mockPersistUserStatsInBulk = jest.fn();
+		await handler(payload, null as unknown as Context);
+		expect(mockPersistUserStatsInBulk).not.toHaveBeenCalled();
+	});
+
+	it('Verifies no user points persisted when there are no users for a ticker', async () => {
+		mockFetchAllTickerConfigs.mockImplementation(() => [{
+			ticker: { S: testTicker },
+			epoch_start_date_utc: { S: '2024-01-16T00:00:00+00:00' },
+			epoch_length_days: { N: '7' },
+			like_multiplier: { N: likeMultiplier.toString() },
+			retweet_multiplier: { N: retweetMultiplier.toString() },
+			view_multiplier: { N: viewMultiplier.toString() },
+			video_view_multiplier: { N: videoViewMultiplier.toString() },
+			quote_multiplier: { N: quoteMultiplier.toString() }
+		}]);
+
+		mockFetchUsersForTicker.mockImplementation(() => []);
+		mockGetCurrentTime.mockImplementation(() => new Date('2024-01-18T00:00:00+00:00'));
+		mockGetCurrentEpochNumber.mockImplementation(() => 1);
+		mockGetCurrentEpochStartDate.mockImplementation(() => new Date('2024-01-16T00:00:00+00:00'));
+
+		mockPersistUserStatsInBulk = jest.fn();
+		await handler(payload, null as unknown as Context);
+		expect(mockPersistUserStatsInBulk).not.toHaveBeenCalled();
+	});
+
+	it('Verifies the handler calculates user points based on 1 tweet', async () => {
+
+		const likeCount = 1;
+		const quoteCount = 2;
+		const retweetCount = 3;
+		const impressionCount = 4;
+		const viewCount = 7;
+
+		mockFetchAllTickerConfigs.mockImplementation(() => [{
+			ticker: { S: testTicker },
+			epoch_start_date_utc: { S: '2024-01-16T00:00:00+00:00' },
+			epoch_length_days: { N: '7' },
+			like_multiplier: { N: likeMultiplier.toString() },
+			retweet_multiplier: { N: retweetMultiplier.toString() },
+			view_multiplier: { N: viewMultiplier.toString() },
+			video_view_multiplier: { N: videoViewMultiplier.toString() },
+			quote_multiplier: { N: quoteMultiplier.toString() }
+		}]);
+
 		mockFetchUserTweetsWithTicker.mockImplementation(() => {
 			return {
 				tweets: [{
 					text: 'this is a tweet about $' + testTicker,
-					public_metrics: { like_count: 1, quote_count: 2, retweet_count: 3, impression_count: 4 },
+					public_metrics: { like_count: likeCount, quote_count: quoteCount, retweet_count: retweetCount, impression_count: impressionCount },
 					attachments: { media_keys: ['123'] }
-				}], includes: { media: [{ public_metrics: { view_count: 7 }, media_key: '123' }] }
+				}], includes: { media: [{ public_metrics: { view_count: viewCount }, media_key: '123' }] }
 			};
 		});
 
+		mockFetchUsersForTicker.mockImplementation(() => [{ twitter_id: twitterId }]);
+		mockGetUsernameById.mockImplementation(() => username);
 		mockGetCurrentTime.mockImplementation(() => new Date('2024-01-18T00:00:00+00:00'));
 		mockGetCurrentEpochNumber.mockImplementation(() => 1);
 		mockGetCurrentEpochStartDate.mockImplementation(() => new Date('2024-01-16T00:00:00+00:00'));
 
 		await handler(payload, null as unknown as Context);
 
-		const likePoints = 2;
-		const quotePoints = 12;
-		const retweetPoints = 9;
-		const viewPoints = 16;
-		const videoViewPoints = 35;
+		const likePoints = likeCount * likeMultiplier;
+		const quotePoints = quoteCount * quoteMultiplier;
+		const retweetPoints = retweetCount * retweetMultiplier;
+		const viewPoints = impressionCount * viewMultiplier;
+		const videoViewPoints = viewCount * videoViewMultiplier;
 		const totalPoints = likePoints + quotePoints + retweetPoints + viewPoints + videoViewPoints;
 
 		expect(mockPersistUserStatsInBulk).toHaveBeenCalledWith([{
@@ -59,7 +123,7 @@ describe('Handler tests', function() {
 			rank: 1
 		}]);
 	});
-});
+})
 
 // Create a sample payload with CloudWatch scheduled event message format
 const payload: ScheduledEvent<any> = {
